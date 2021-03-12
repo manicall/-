be525f60ba393b7532f255c9e1c81da9971ce59d
max-pc <makskms@gmail.com>
2021-03-13 00:14:03 +1000

    1.2v1

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,17 +1,53 @@
 #include <iostream>
-
-int a(int n, int i);
-
+#include <vector>
+#include <math.h>
+using namespace std;
+//int n = 3;
+//double x[3] = {9.66, 1.84, -0.50 };
+int n = 2;
+double x[2] = { -1, -9 };
+double a(int n, int i);
+void check(double x, vector<double> c);
+void full_check(double* x, vector<double> c);
 int main() {
 	setlocale(LC_ALL, "ru");
+	vector<double> coefficients;
+	cout << "Коэффициенты: ";
+	for (int i = 0; i <= n; i++)
+		coefficients.push_back(a(n, n - i));
+	for (auto x : coefficients)
+		cout << x << " ";
+	cout << endl << "Проверка: " << endl; 
 
-
+	full_check(x, coefficients);
 }
 
-int a(int n, int i) {
-	double x = -9;
+double a(int n, int i) {
+	double temp;
+	if (i == n) {
+		temp = 1.0;
+	}
+	else if (i == 0) {
+		temp = -a(n - 1, 0) * x[n-1];
+	}
+	else if (i >= 1 && i < n - 1) {
+		temp = a(n - 1, i - 1) - a(n - 1, i) * x[n-1];
+	}
+	else if (i == n - 1) {
+		temp = a(n - 1, n - 2) - x[n-1];
+	}
 
-	if (n > 0 && i == 0)
-		return x * a(n - 1, 0);
+	return temp;
+}
 
+void check(double x, vector<double> c) {
+	double sum = 0;
+	for (int i = n; i >= 0; i--)
+		sum += pow(x, i) * c[c.size() - i - 1];
+	cout <<"x = " << x << "; P(x) = " << sum << endl;
+};
+
+void full_check(double* x, vector<double> c) {
+	for (int i = 0; i < n; i++)
+		check(x[i], c);
 }
